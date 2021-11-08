7cd6da9aab96badd2daefeabe222b9092921433f
arjuna90111-jpg <arjuna90111@gmail.com>
2021-11-08 21:24:48 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const content = document.getElementById('content');
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+const addedBtn = document.getElementById('addedBtn');
+const deletedBtn = document.getElementById('deletedBtn');
+const list = document.getElementById('list');
+
+
+const listContent = [];
+
+function render() {
+    let htmlStr = ''
+
+
+    listContent.forEach(function (item) {
+        htmlStr = htmlStr + `
+        <div class="item">
+            <div>
+                <p>內容:${item.content}</p>
+                <p>時間:${item.date} ${item.time}</p>
+            </div>
+        </div>
+        `
+        list.innerHTML = htmlStr;
+
+    })
+
+}
+
+addedBtn.addEventListener('click', function () {
+    // console.log(content.value);
+    // console.log(date.value);
+    // console.log(time.value);
+
+    listContent.unshift({
+        content: content.value,
+        date: date.value,
+        time: time.value
+    })
+
+    // console.log(listContent);
+
+    // listContent.forEach(function (item, i) {
+    //     console.log('item',item);
+
+    // })
+
+    // for (let i = 0; i < listContent.length; i++) {
+
+
+    // }
+
+
+    render();
+})
+
+
+
+deletedBtn.addEventListener('click', function () {
+
+    listContent.shift()
+
+    render();
+})
